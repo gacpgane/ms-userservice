e8bd92db97aff11fdf634d4546b86b854a07ed1b
Prabuddha Ganegoda <prabuddhaganegoda@Prabuddhas-MacBook-Air.local>
2025-03-02 13:26:11 +1100

    code checking

---
+++ src/main/java/com/cpg/ms_userservice/MsUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cpg.ms_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserserviceApplication.class, args);
+	}
+
+}
